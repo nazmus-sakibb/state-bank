11a891592f89e6ffd14a51f8066b4b1073e1d469
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-23 23:44:00 +0600

    get deposit and withdraw field

--- functional-state-bank/js/deposit.js
+++ functional-state-bank/js/deposit.js
@@ -1,5 +1,6 @@
 function getInputValueById(inputId){
-    
+    const inputField = document.getElementById(inputId);
+    return inputField;
 }
 
 document.getElementById('btn-deposit').addEventListener('click', function(){
