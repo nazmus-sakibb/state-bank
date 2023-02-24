83ee44f1359f07284c6f04f8f31656dd34b6df8b
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-24 15:41:38 +0600

    set deposit total value

--- functional-state-bank/js/deposit.js
+++ functional-state-bank/js/deposit.js
@@ -11,6 +11,11 @@ function getElementValueById(elementId){
     return elementValue;
 }
 
+function setTextElementValueById(elementId, newValue){
+    const textElement = document.getElementById(elementId);
+    elementId.innerText = newValue;
+}
+
 document.getElementById('btn-deposit').addEventListener('click', function(){
     const newDepositAmount = getInputFieldValueById('deposit-field');
 
@@ -21,4 +26,6 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     // calculate new deposit total
     const newDepositTotal = previousDepositTotal + newDepositAmount;
     
+    // set deposit total value
+    setTextElementValueById('deposit-total', newDepositTotal);
 })
