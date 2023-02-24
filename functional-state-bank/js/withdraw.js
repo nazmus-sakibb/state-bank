cf9960bb08e3f7aed7a8666ce4827db167e89628
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-24 18:02:19 +0600

    calculate and set the withdraw value

--- functional-state-bank/js/withdraw.js
+++ functional-state-bank/js/withdraw.js
@@ -1,6 +1,10 @@
 /**
  * 1. add withdraw button event handler
+ * 2. get withdraw amount by getInputValueById function
+ * 3. get previous withdraw amount
+ * 4. calculate and set the withdraw total
  */
 document.getElementById('btn-withdraw').addEventListener('click', function(){
-
+    const newWithdrawAmount = getInputFieldValueById('withdraw-field');
+    const previousWithdrawTotal = getElementValueById('withdraw-total');
 })
