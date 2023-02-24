b90f7799b64b209fad2c97053038ca38a225fe43
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-24 18:28:53 +0600

    calculate and set the new balance

--- functional-state-bank/js/withdraw.js
+++ functional-state-bank/js/withdraw.js
@@ -3,8 +3,14 @@
  * 2. get withdraw amount by getInputValueById function
  * 3. get previous withdraw amount
  * 4. calculate and set the withdraw total
+ * 5. get previous balance total
  */
 document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithdrawAmount = getInputFieldValueById('withdraw-field');
     const previousWithdrawTotal = getElementValueById('withdraw-total');
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    setTextElementValueById('withdraw-total', newWithdrawTotal);
+    const previousBalanceTotal = getElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
 })
