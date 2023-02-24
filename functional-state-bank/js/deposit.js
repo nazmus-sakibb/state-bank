f434ebdb273fc200771a51f594f3b97bb42fa79a
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-24 15:25:34 +0600

    get previous deposit total

--- functional-state-bank/js/deposit.js
+++ functional-state-bank/js/deposit.js
@@ -1,9 +1,21 @@
 function getInputFieldValueById(inputId){
     const inputField = document.getElementById(inputId);
     const inputFieldValue = parseFloat(inputField.value);
+    inputField.value = '';
     return inputFieldValue;
+}
+
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValue = parseFloat(element.innerText);
+    element.innerText = '';
+    return elementValue;
 }
 
 document.getElementById('btn-deposit').addEventListener('click', function(){
     const newDepositAmount = getInputFieldValueById('deposit-feild');
+
+    // get previous deposit total
+    const previousDepositTotal = getElementValueById('deposit-total');
+    console.log(newDepositAmount, previousDepositTota);
 })
