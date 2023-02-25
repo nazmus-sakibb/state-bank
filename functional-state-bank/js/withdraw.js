49fec9dd3dbe82f55ee0d19a28367f4f0be7fc99
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-25 22:55:44 +0600

    calculte total balance

--- functional-state-bank/js/withdraw.js
+++ functional-state-bank/js/withdraw.js
@@ -13,5 +13,4 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const previousBalanceTotal = getElementValueById('balance-total');
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     setTextElementValueById('balance-total', newBalanceTotal);
-    setTextElementValueById('balance-total', newBalanceTotal);
 })
